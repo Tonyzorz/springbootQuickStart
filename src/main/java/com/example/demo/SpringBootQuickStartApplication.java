4f028f93dd21efed7660bcad8c68fa9710a54f75
taewon <tonyzorz@naver.com>
2019-08-12 20:52:12 +0900

    104

--- src/main/java/com/example/demo/SpringBootQuickStartApplication.java
+++ src/main/java/com/example/demo/SpringBootQuickStartApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages= {"com.example.demo","com.ruby"})
+@ComponentScan(basePackages= {"com.example.demo","com.ruby", "com.rubypaper"})
 public class SpringBootQuickStartApplication {
 
 	public static void main(String[] args) {
